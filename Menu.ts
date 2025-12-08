18e9ec0b4e3afdd3dca1b4ed74d797c9b16b6bc3
Renato Sales <renatosalespaula@gmail.com>
2025-12-08 15:21:59 -0300

    implementação da classe Conta

--- Menu.ts
+++ Menu.ts
@@ -1,8 +1,16 @@
 import readlinesync = require("readline-sync");
 import { colors } from "./src/util/Colors";
+import { Conta } from "./src/model/Conta";
 
 export function main() {
   let opcao: number;
+
+  const conta: Conta = new Conta(1, 123, 1, "Adriana", 10000);
+  conta.visualizar();
+  conta.sacar(10500);
+  conta.visualizar();
+  conta.depositar(5000);
+  conta.visualizar();
 
   while (true) {
     console.log(
